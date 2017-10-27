261bb8fad135cb53c91d6a2c90d5bcf81ee52414
Jacob Williams <ponyboy47@gmail.com>
2017-10-27 17:41:04 -0600

    Made everything publicly accessible

--- Sources/ErrNo+Glibc.swift
+++ Sources/ErrNo+Glibc.swift
@@ -6,14 +6,14 @@ import Glibc
 /// Wrapper around errno values from the C standard library.
 public extension ErrNo {
     /// Resource temporarily unavailable (POSIX.1)
-    static let EAGAIN = ErrNo(rawValue: Glibc.EAGAIN)
+    public static let EAGAIN = ErrNo(rawValue: Glibc.EAGAIN)
 	/// Resource deadlock would occur
-	static let EDEADLK = ErrNo(rawValue: Glibc.EDEADLK)
-    static let EDEADLOCK = ErrNo.EDEADLK
+	public static let EDEADLK = ErrNo(rawValue: Glibc.EDEADLK)
+    public static let EDEADLOCK = ErrNo.EDEADLK
 	/// File name too long
-	static let ENAMETOOLONG = ErrNo(rawValue: Glibc.ENAMETOOLONG)
+	public static let ENAMETOOLONG = ErrNo(rawValue: Glibc.ENAMETOOLONG)
 	/// No record locks available
-	static let ENOLCK = ErrNo(rawValue: Glibc.ENOLCK)
+	public static let ENOLCK = ErrNo(rawValue: Glibc.ENOLCK)
 
 /*
  * This error code is special: arch syscall entry code will return
@@ -23,199 +23,199 @@ public extension ErrNo {
  * implementations should refrain from returning -ENOSYS.
  */
 	/// Invalid system call number
-	static let ENOSYS = ErrNo(rawValue: Glibc.ENOSYS)
+	public static let ENOSYS = ErrNo(rawValue: Glibc.ENOSYS)
 
 	/// Directory not empty
-	static let ENOTEMPTY = ErrNo(rawValue: Glibc.ENOTEMPTY)
+	public static let ENOTEMPTY = ErrNo(rawValue: Glibc.ENOTEMPTY)
 	/// Too many symbolic links encountered
-	static let ELOOP = ErrNo(rawValue: Glibc.ELOOP)
+	public static let ELOOP = ErrNo(rawValue: Glibc.ELOOP)
 	/// No message of desired type
-	static let ENOMSG = ErrNo(rawValue: Glibc.ENOMSG)
+	public static let ENOMSG = ErrNo(rawValue: Glibc.ENOMSG)
 	/// Identifier removed
-	static let EIDRM = ErrNo(rawValue: Glibc.EIDRM)
+	public static let EIDRM = ErrNo(rawValue: Glibc.EIDRM)
 	/// Channel number out of range
-	static let ECHRNG = ErrNo(rawValue: Glibc.ECHRNG)
+	public static let ECHRNG = ErrNo(rawValue: Glibc.ECHRNG)
 	/// Level 2 not synchronized
-	static let EL2NSYNC = ErrNo(rawValue: Glibc.EL2NSYNC)
+	public static let EL2NSYNC = ErrNo(rawValue: Glibc.EL2NSYNC)
 	/// Level 3 halted
-	static let EL3HLT = ErrNo(rawValue: Glibc.EL3HLT)
+	public static let EL3HLT = ErrNo(rawValue: Glibc.EL3HLT)
 	/// Level 3 reset
-	static let EL3RST = ErrNo(rawValue: Glibc.EL3RST)
+	public static let EL3RST = ErrNo(rawValue: Glibc.EL3RST)
 	/// Link number out of range
-	static let ELNRNG = ErrNo(rawValue: Glibc.ELNRNG)
+	public static let ELNRNG = ErrNo(rawValue: Glibc.ELNRNG)
 	/// Protocol driver not attached
-	static let EUNATCH = ErrNo(rawValue: Glibc.EUNATCH)
+	public static let EUNATCH = ErrNo(rawValue: Glibc.EUNATCH)
 	/// No CSI structure available
-	static let ENOCSI = ErrNo(rawValue: Glibc.ENOCSI)
+	public static let ENOCSI = ErrNo(rawValue: Glibc.ENOCSI)
 	/// Level 2 halted
-	static let EL2HLT = ErrNo(rawValue: Glibc.EL2HLT)
+	public static let EL2HLT = ErrNo(rawValue: Glibc.EL2HLT)
 	/// Invalid exchange
-	static let EBADE = ErrNo(rawValue: Glibc.EBADE)
+	public static let EBADE = ErrNo(rawValue: Glibc.EBADE)
 	/// Invalid request descriptor
-	static let EBADR = ErrNo(rawValue: Glibc.EBADR)
+	public static let EBADR = ErrNo(rawValue: Glibc.EBADR)
 	/// Exchange full
-	static let EXFULL = ErrNo(rawValue: Glibc.EXFULL)
+	public static let EXFULL = ErrNo(rawValue: Glibc.EXFULL)
 	/// No anode
-	static let ENOANO = ErrNo(rawValue: Glibc.ENOANO)
+	public static let ENOANO = ErrNo(rawValue: Glibc.ENOANO)
 	/// Invalid request code
-	static let EBADRQC = ErrNo(rawValue: Glibc.EBADRQC)
+	public static let EBADRQC = ErrNo(rawValue: Glibc.EBADRQC)
 	/// Invalid slot
-	static let EBADSLT = ErrNo(rawValue: Glibc.EBADSLT)
+	public static let EBADSLT = ErrNo(rawValue: Glibc.EBADSLT)
 
 	/// Bad font file format
-	static let EBFONT = ErrNo(rawValue: Glibc.EBFONT)
+	public static let EBFONT = ErrNo(rawValue: Glibc.EBFONT)
 	/// Device not a stream
-	static let ENOSTR = ErrNo(rawValue: Glibc.ENOSTR)
+	public static let ENOSTR = ErrNo(rawValue: Glibc.ENOSTR)
 	/// No data available
-	static let ENODATA = ErrNo(rawValue: Glibc.ENODATA)
+	public static let ENODATA = ErrNo(rawValue: Glibc.ENODATA)
 	/// Timer expired
-	static let ETIME = ErrNo(rawValue: Glibc.ETIME)
+	public static let ETIME = ErrNo(rawValue: Glibc.ETIME)
 	/// Out of streams resources
-	static let ENOSR = ErrNo(rawValue: Glibc.ENOSR)
+	public static let ENOSR = ErrNo(rawValue: Glibc.ENOSR)
 	/// Machine is not on the network
-	static let ENONET = ErrNo(rawValue: Glibc.ENONET)
+	public static let ENONET = ErrNo(rawValue: Glibc.ENONET)
 	/// Package not installed
-	static let ENOPKG = ErrNo(rawValue: Glibc.ENOPKG)
+	public static let ENOPKG = ErrNo(rawValue: Glibc.ENOPKG)
 	/// Object is remote
-	static let EREMOTE = ErrNo(rawValue: Glibc.EREMOTE)
+	public static let EREMOTE = ErrNo(rawValue: Glibc.EREMOTE)
 	/// Link has been severed
-	static let ENOLINK = ErrNo(rawValue: Glibc.ENOLINK)
+	public static let ENOLINK = ErrNo(rawValue: Glibc.ENOLINK)
 	/// Advertise error
-	static let EADV = ErrNo(rawValue: Glibc.EADV)
+	public static let EADV = ErrNo(rawValue: Glibc.EADV)
 	/// Srmount error
-	static let ESRMNT = ErrNo(rawValue: Glibc.ESRMNT)
+	public static let ESRMNT = ErrNo(rawValue: Glibc.ESRMNT)
 	/// Communication error on send
-	static let ECOMM = ErrNo(rawValue: Glibc.ECOMM)
+	public static let ECOMM = ErrNo(rawValue: Glibc.ECOMM)
 	/// Protocol error
-	static let EPROTO = ErrNo(rawValue: Glibc.EPROTO)
+	public static let EPROTO = ErrNo(rawValue: Glibc.EPROTO)
 	/// Multihop attempted
-	static let EMULTIHOP = ErrNo(rawValue: Glibc.EMULTIHOP)
+	public static let EMULTIHOP = ErrNo(rawValue: Glibc.EMULTIHOP)
 	/// RFS specific error
-	static let EDOTDOT = ErrNo(rawValue: Glibc.EDOTDOT)
+	public static let EDOTDOT = ErrNo(rawValue: Glibc.EDOTDOT)
 	/// Not a data message
-	static let EBADMSG = ErrNo(rawValue: Glibc.EBADMSG)
+	public static let EBADMSG = ErrNo(rawValue: Glibc.EBADMSG)
 	/// Value too large for defined data type
-	static let EOVERFLOW = ErrNo(rawValue: Glibc.EOVERFLOW)
+	public static let EOVERFLOW = ErrNo(rawValue: Glibc.EOVERFLOW)
 	/// Name not unique on network
-	static let ENOTUNIQ = ErrNo(rawValue: Glibc.ENOTUNIQ)
+	public static let ENOTUNIQ = ErrNo(rawValue: Glibc.ENOTUNIQ)
 	/// File descriptor in bad state
-	static let EBADFD = ErrNo(rawValue: Glibc.EBADFD)
+	public static let EBADFD = ErrNo(rawValue: Glibc.EBADFD)
 	/// Remote address changed
-	static let EREMCHG = ErrNo(rawValue: Glibc.EREMCHG)
+	public static let EREMCHG = ErrNo(rawValue: Glibc.EREMCHG)
 	/// Can not access a needed shared library
-	static let ELIBACC = ErrNo(rawValue: Glibc.ELIBACC)
+	public static let ELIBACC = ErrNo(rawValue: Glibc.ELIBACC)
 	/// Accessing a corrupted shared library
-	static let ELIBBAD = ErrNo(rawValue: Glibc.ELIBBAD)
+	public static let ELIBBAD = ErrNo(rawValue: Glibc.ELIBBAD)
 	/// .lib section in a.out corrupted
-	static let ELIBSCN = ErrNo(rawValue: Glibc.ELIBSCN)
+	public static let ELIBSCN = ErrNo(rawValue: Glibc.ELIBSCN)
 	/// Attempting to link in too many shared libraries
-	static let ELIBMAX = ErrNo(rawValue: Glibc.ELIBMAX)
+	public static let ELIBMAX = ErrNo(rawValue: Glibc.ELIBMAX)
 	/// Cannot exec a shared library directly
-	static let ELIBEXEC = ErrNo(rawValue: Glibc.ELIBEXEC)
+	public static let ELIBEXEC = ErrNo(rawValue: Glibc.ELIBEXEC)
 	/// Illegal byte sequence
-	static let EILSEQ = ErrNo(rawValue: Glibc.EILSEQ)
+	public static let EILSEQ = ErrNo(rawValue: Glibc.EILSEQ)
 	/// Interrupted system call should be restarted
-	static let ERESTART = ErrNo(rawValue: Glibc.ERESTART)
+	public static let ERESTART = ErrNo(rawValue: Glibc.ERESTART)
 	/// Streams pipe error
-	static let ESTRPIPE = ErrNo(rawValue: Glibc.ESTRPIPE)
+	public static let ESTRPIPE = ErrNo(rawValue: Glibc.ESTRPIPE)
 	/// Too many users
-	static let EUSERS = ErrNo(rawValue: Glibc.EUSERS)
+	public static let EUSERS = ErrNo(rawValue: Glibc.EUSERS)
 	/// Socket operation on non-socket
-	static let ENOTSOCK = ErrNo(rawValue: Glibc.ENOTSOCK)
+	public static let ENOTSOCK = ErrNo(rawValue: Glibc.ENOTSOCK)
 	/// Destination address required
-	static let EDESTADDRREQ = ErrNo(rawValue: Glibc.EDESTADDRREQ)
+	public static let EDESTADDRREQ = ErrNo(rawValue: Glibc.EDESTADDRREQ)
 	/// Message too long
-	static let EMSGSIZE = ErrNo(rawValue: Glibc.EMSGSIZE)
+	public static let EMSGSIZE = ErrNo(rawValue: Glibc.EMSGSIZE)
 	/// Protocol wrong type for socket
-	static let EPROTOTYPE = ErrNo(rawValue: Glibc.EPROTOTYPE)
+	public static let EPROTOTYPE = ErrNo(rawValue: Glibc.EPROTOTYPE)
 	/// Protocol not available
-	static let ENOPROTOOPT = ErrNo(rawValue: Glibc.ENOPROTOOPT)
+	public static let ENOPROTOOPT = ErrNo(rawValue: Glibc.ENOPROTOOPT)
 	/// Protocol not supported
-	static let EPROTONOSUPPORT = ErrNo(rawValue: Glibc.EPROTONOSUPPORT)
+	public static let EPROTONOSUPPORT = ErrNo(rawValue: Glibc.EPROTONOSUPPORT)
 	/// Socket type not supported
-	static let ESOCKTNOSUPPORT = ErrNo(rawValue: Glibc.ESOCKTNOSUPPORT)
+	public static let ESOCKTNOSUPPORT = ErrNo(rawValue: Glibc.ESOCKTNOSUPPORT)
 	/// Operation not supported on transport endpoint
-	static let EOPNOTSUPP = ErrNo(rawValue: Glibc.EOPNOTSUPP)
+	public static let EOPNOTSUPP = ErrNo(rawValue: Glibc.EOPNOTSUPP)
 	/// Protocol family not supported
-	static let EPFNOSUPPORT = ErrNo(rawValue: Glibc.EPFNOSUPPORT)
+	public static let EPFNOSUPPORT = ErrNo(rawValue: Glibc.EPFNOSUPPORT)
 	/// Address family not supported by protocol
-	static let EAFNOSUPPORT = ErrNo(rawValue: Glibc.EAFNOSUPPORT)
+	public static let EAFNOSUPPORT = ErrNo(rawValue: Glibc.EAFNOSUPPORT)
 	/// Address already in use
-	static let EADDRINUSE = ErrNo(rawValue: Glibc.EADDRINUSE)
+	public static let EADDRINUSE = ErrNo(rawValue: Glibc.EADDRINUSE)
 	/// Cannot assign requested address
-	static let EADDRNOTAVAIL = ErrNo(rawValue: Glibc.EADDRNOTAVAIL)
+	public static let EADDRNOTAVAIL = ErrNo(rawValue: Glibc.EADDRNOTAVAIL)
 	/// Network is down
-	static let ENETDOWN = ErrNo(rawValue: Glibc.ENETDOWN)
+	public static let ENETDOWN = ErrNo(rawValue: Glibc.ENETDOWN)
 	/// Network is unreachable
-	static let ENETUNREACH = ErrNo(rawValue: Glibc.ENETUNREACH)
+	public static let ENETUNREACH = ErrNo(rawValue: Glibc.ENETUNREACH)
 	/// Network dropped connection because of reset
-	static let ENETRESET = ErrNo(rawValue: Glibc.ENETRESET)
+	public static let ENETRESET = ErrNo(rawValue: Glibc.ENETRESET)
 	/// Software caused connection abort
-	static let ECONNABORTED = ErrNo(rawValue: Glibc.ECONNABORTED)
+	public static let ECONNABORTED = ErrNo(rawValue: Glibc.ECONNABORTED)
 	/// Connection reset by peer
-	static let ECONNRESET = ErrNo(rawValue: Glibc.ECONNRESET)
+	public static let ECONNRESET = ErrNo(rawValue: Glibc.ECONNRESET)
 	/// No buffer space available
-	static let ENOBUFS = ErrNo(rawValue: Glibc.ENOBUFS)
+	public static let ENOBUFS = ErrNo(rawValue: Glibc.ENOBUFS)
 	/// Transport endpoint is already connected
-	static let EISCONN = ErrNo(rawValue: Glibc.EISCONN)
+	public static let EISCONN = ErrNo(rawValue: Glibc.EISCONN)
 	/// Transport endpoint is not connected
-	static let ENOTCONN = ErrNo(rawValue: Glibc.ENOTCONN)
+	public static let ENOTCONN = ErrNo(rawValue: Glibc.ENOTCONN)
 	/// Cannot send after transport endpoint shutdown
-	static let ESHUTDOWN = ErrNo(rawValue: Glibc.ESHUTDOWN)
+	public static let ESHUTDOWN = ErrNo(rawValue: Glibc.ESHUTDOWN)
 	/// Too many references: cannot splice
-	static let ETOOMANYREFS = ErrNo(rawValue: Glibc.ETOOMANYREFS)
+	public static let ETOOMANYREFS = ErrNo(rawValue: Glibc.ETOOMANYREFS)
 	/// Connection timed out
-	static let ETIMEDOUT = ErrNo(rawValue: Glibc.ETIMEDOUT)
+	public static let ETIMEDOUT = ErrNo(rawValue: Glibc.ETIMEDOUT)
 	/// Connection refused
-	static let ECONNREFUSED = ErrNo(rawValue: Glibc.ECONNREFUSED)
+	public static let ECONNREFUSED = ErrNo(rawValue: Glibc.ECONNREFUSED)
 	/// Host is down
-	static let EHOSTDOWN = ErrNo(rawValue: Glibc.EHOSTDOWN)
+	public static let EHOSTDOWN = ErrNo(rawValue: Glibc.EHOSTDOWN)
 	/// No route to host
-	static let EHOSTUNREACH = ErrNo(rawValue: Glibc.EHOSTUNREACH)
+	public static let EHOSTUNREACH = ErrNo(rawValue: Glibc.EHOSTUNREACH)
 	/// Operation already in progress
-	static let EALREADY = ErrNo(rawValue: Glibc.EALREADY)
+	public static let EALREADY = ErrNo(rawValue: Glibc.EALREADY)
 	/// Operation now in progress
-	static let EINPROGRESS = ErrNo(rawValue: Glibc.EINPROGRESS)
+	public static let EINPROGRESS = ErrNo(rawValue: Glibc.EINPROGRESS)
 	/// Stale file handle
-	static let ESTALE = ErrNo(rawValue: Glibc.ESTALE)
+	public static let ESTALE = ErrNo(rawValue: Glibc.ESTALE)
 	/// Structure needs cleaning
-	static let EUCLEAN = ErrNo(rawValue: Glibc.EUCLEAN)
+	public static let EUCLEAN = ErrNo(rawValue: Glibc.EUCLEAN)
 	/// Not a XENIX named type file
-	static let ENOTNAM = ErrNo(rawValue: Glibc.ENOTNAM)
+	public static let ENOTNAM = ErrNo(rawValue: Glibc.ENOTNAM)
 	/// No XENIX semaphores available
-	static let ENAVAIL = ErrNo(rawValue: Glibc.ENAVAIL)
+	public static let ENAVAIL = ErrNo(rawValue: Glibc.ENAVAIL)
 	/// Is a named type file
-	static let EISNAM = ErrNo(rawValue: Glibc.EISNAM)
+	public static let EISNAM = ErrNo(rawValue: Glibc.EISNAM)
 	/// Remote I/O error
-	static let EREMOTEIO = ErrNo(rawValue: Glibc.EREMOTEIO)
+	public static let EREMOTEIO = ErrNo(rawValue: Glibc.EREMOTEIO)
 	/// Quota exceeded
-	static let EDQUOT = ErrNo(rawValue: Glibc.EDQUOT)
+	public static let EDQUOT = ErrNo(rawValue: Glibc.EDQUOT)
 
 	/// No medium found
-	static let ENOMEDIUM = ErrNo(rawValue: Glibc.ENOMEDIUM)
+	public static let ENOMEDIUM = ErrNo(rawValue: Glibc.ENOMEDIUM)
 	/// Wrong medium type
-	static let EMEDIUMTYPE = ErrNo(rawValue: Glibc.EMEDIUMTYPE)
+	public static let EMEDIUMTYPE = ErrNo(rawValue: Glibc.EMEDIUMTYPE)
 	/// Operation Canceled
-	static let ECANCELED = ErrNo(rawValue: Glibc.ECANCELED)
+	public static let ECANCELED = ErrNo(rawValue: Glibc.ECANCELED)
 	/// Required key not available
-	static let ENOKEY = ErrNo(rawValue: Glibc.ENOKEY)
+	public static let ENOKEY = ErrNo(rawValue: Glibc.ENOKEY)
 	/// Key has expired
-	static let EKEYEXPIRED = ErrNo(rawValue: Glibc.EKEYEXPIRED)
+	public static let EKEYEXPIRED = ErrNo(rawValue: Glibc.EKEYEXPIRED)
 	/// Key has been revoked
-	static let EKEYREVOKED = ErrNo(rawValue: Glibc.EKEYREVOKED)
+	public static let EKEYREVOKED = ErrNo(rawValue: Glibc.EKEYREVOKED)
 	/// Key was rejected by service
-	static let EKEYREJECTED = ErrNo(rawValue: Glibc.EKEYREJECTED)
+	public static let EKEYREJECTED = ErrNo(rawValue: Glibc.EKEYREJECTED)
 
 /* for robust mutexes */
 	/// Owner died
-	static let EOWNERDEAD = ErrNo(rawValue: Glibc.EOWNERDEAD)
+	public static let EOWNERDEAD = ErrNo(rawValue: Glibc.EOWNERDEAD)
 	/// State not recoverable
-	static let ENOTRECOVERABLE = ErrNo(rawValue: Glibc.ENOTRECOVERABLE)
+	public static let ENOTRECOVERABLE = ErrNo(rawValue: Glibc.ENOTRECOVERABLE)
 
 	/// Operation not possible due to RF-kill
-	static let ERFKILL = ErrNo(rawValue: Glibc.ERFKILL)
+	public static let ERFKILL = ErrNo(rawValue: Glibc.ERFKILL)
 
 	/// Memory page has hardware error
-	static let EHWPOISON = ErrNo(rawValue: Glibc.EHWPOISON)
+	public static let EHWPOISON = ErrNo(rawValue: Glibc.EHWPOISON)
 }
 #endif
--- Sources/ErrNo.swift
+++ Sources/ErrNo.swift
@@ -34,77 +34,77 @@ public struct ErrNo: Error, RawRepresentable, Equatable, Comparable {
         self.rawValue = rawValue
     }
 
-    public static func <(lhs: ErrNo, rhs: ErrNo) -> Bool {
+    public public static func <(lhs: ErrNo, rhs: ErrNo) -> Bool {
         return lhs.rawValue < rhs.rawValue
     }
 
     /// Operation not permitted (POSIX.1)
-	static let EPERM = 1
+	public static let EPERM = 1
     /// No such file or directory (POSIX.1)
-	static let ENOENT = 2
+	public static let ENOENT = 2
     /// No such process (POSIX.1)
-	static let ESRCH = 3
+	public static let ESRCH = 3
     /// Interrupted system call (POSIX.1)
-	static let EINTR = 4
+	public static let EINTR = 4
     /// I/O error (POSIX.1)
-	static let EIO = 5
+	public static let EIO = 5
     /// No such device or address (POSIX.1)
-	static let ENXIO = 6
+	public static let ENXIO = 6
     /// Argument list too long (POSIX.1)
-	static let E2BIG = 7
+	public static let E2BIG = 7
     /// Exec format error (POSIX.1)
-	static let ENOEXEC = 8
+	public static let ENOEXEC = 8
     /// Bad file descriptor (POSIX.1)
-	static let EBADF = 9
+	public static let EBADF = 9
     /// No child processes (POSIX.1)
-	static let ECHILD = 10
+	public static let ECHILD = 10
     /// Out of memory (POSIX.1)
-	static let ENOMEM = 12
+	public static let ENOMEM = 12
     /// Permission denied (POSIX.1)
-	static let EACCES = 13
+	public static let EACCES = 13
     /// Bad address (POSIX.1)
-	static let EFAULT = 14
+	public static let EFAULT = 14
     /// Block device required (POSIX.1)
-	static let ENOTBLK = 15
+	public static let ENOTBLK = 15
     /// Device or resource busy (POSIX.1)
-	static let EBUSY = 16
+	public static let EBUSY = 16
     /// File exists (POSIX.1)
-	static let EEXIST = 17
+	public static let EEXIST = 17
     /// Improper link (POSIX.1)
-	static let EXDEV = 18
+	public static let EXDEV = 18
     /// No such device (POSIX.1)
-	static let ENODEV = 19
+	public static let ENODEV = 19
     /// Not a directory (POSIX.1)
-	static let ENOTDIR = 20
+	public static let ENOTDIR = 20
     /// Is a directory (POSIX.1)
-	static let EISDIR = 21
+	public static let EISDIR = 21
     /// Invalid argument (POSIX.1)
-	static let EINVAL = 22
+	public static let EINVAL = 22
     /// Too many open files in system (POSIX.1);  on  Linux,  this  is  probably  a  result  of  encountering  the /proc/sys/fs/file-max limit (see proc(5)).
-	static let ENFILE = 23
+	public static let ENFILE = 23
     /// Too many open files (POSIX.1); Commonly caused by exceeding the RLIMIT_NOFILE resource limit described in getrlimit(2).
-	static let EMFILE = 24
+	public static let EMFILE = 24
     /// Inappropriate I/O control operation (POSIX.1)
-	static let ENOTTY = 25
+	public static let ENOTTY = 25
     /// Text file busy (POSIX.1)
-	static let ETXTBSY = 26
+	public static let ETXTBSY = 26
     /// File too large (POSIX.1)
-	static let EFBIG = 27
+	public static let EFBIG = 27
     /// No space left on device (POSIX.1)
-	static let ENOSPC = 28
+	public static let ENOSPC = 28
     /// Illegal seek (POSIX.1)
-	static let ESPIPE = 29
+	public static let ESPIPE = 29
     /// Read-only file system (POSIX.1)
-	static let EROFS = 30
+	public static let EROFS = 30
     /// Too many links (POSIX.1)
-	static let EMLINK = 31
+	public static let EMLINK = 31
     /// Broken pipe (POSIX.1)
-	static let EPIPE = 32
+	public static let EPIPE = 32
     /// Math argument out of domain of function (POSIX.1, C99)
-	static let EDOM = 33
+	public static let EDOM = 33
     /// Math result not representable, result too large (POSIX.1, C99)
-	static let ERANGE = 34
+	public static let ERANGE = 34
 
     /// Operation would block
-	static let EWOULDBLOCK = ErrNo.EAGAIN
+	public static let EWOULDBLOCK = ErrNo.EAGAIN
 }

6a6480c993adb989f7382816a2613b9785fcab1e
Jacob Williams <ponyboy47@gmail.com>
2017-10-27 17:18:01 -0600

    Fixed compile errors on macOS

--- Sources/ErrNo+Darwin.swift
+++ Sources/ErrNo+Darwin.swift
@@ -6,179 +6,166 @@ import Darwin.C.errno
 public extension ErrNo {
 /* non-blocking and interrupt i/o */
 	/// Resource temporarily unavailable
-	static let EAGAIN = ErrNo(rawValue: Darwin.C.errno.EAGAIN)
+	static let EAGAIN = ErrNo(rawValue: Darwin.EAGAIN)
 	/// Operation now in progress
-	static let EINPROGRESS = ErrNo(rawValue: Darwin.C.errno.EINPROGRESS)
+	static let EINPROGRESS = ErrNo(rawValue: Darwin.EINPROGRESS)
 	/// Operation already in progress
-	static let EALREADY = ErrNo(rawValue: Darwin.C.errno.EALREADY)
+	static let EALREADY = ErrNo(rawValue: Darwin.EALREADY)
 
 /* ipc/network software -- argument errors */
 	/// Socket operation on non-socket
-	static let ENOTSOCK = ErrNo(rawValue: Darwin.C.errno.ENOTSOCK)
+	static let ENOTSOCK = ErrNo(rawValue: Darwin.ENOTSOCK)
 	/// Destination address required
-	static let EDESTADDRREQ = ErrNo(rawValue: Darwin.C.errno.EDESTADDRREQ)
+	static let EDESTADDRREQ = ErrNo(rawValue: Darwin.EDESTADDRREQ)
 	/// Message too long
-	static let EMSGSIZE = ErrNo(rawValue: Darwin.C.errno.EMSGSIZE)
+	static let EMSGSIZE = ErrNo(rawValue: Darwin.EMSGSIZE)
 	/// Protocol wrong type for socket
-	static let EPROTOTYPE = ErrNo(rawValue: Darwin.C.errno.EPROTOTYPE)
+	static let EPROTOTYPE = ErrNo(rawValue: Darwin.EPROTOTYPE)
 	/// Protocol not available
-	static let ENOPROTOOPT = ErrNo(rawValue: Darwin.C.errno.ENOPROTOOPT)
+	static let ENOPROTOOPT = ErrNo(rawValue: Darwin.ENOPROTOOPT)
 	/// Protocol not supported
-	static let EPROTONOSUPPORT = ErrNo(rawValue: Darwin.C.errno.EPROTONOSUPPORT)
+	static let EPROTONOSUPPORT = ErrNo(rawValue: Darwin.EPROTONOSUPPORT)
 	/// Socket type not supported
-	static let ESOCKTNOSUPPORT = ErrNo(rawValue: Darwin.C.errno.ESOCKTNOSUPPORT)
+	static let ESOCKTNOSUPPORT = ErrNo(rawValue: Darwin.ESOCKTNOSUPPORT)
 	/// Operation not supported
-	static let ENOTSUP = ErrNo(rawValue: Darwin.C.errno.ENOTSUP)
+	static let ENOTSUP = ErrNo(rawValue: Darwin.ENOTSUP)
     /// Operation not supported on socket
     static let EOPNOTSUPP = ErrNo.ENOTSUP
 	/// Protocol family not supported
-	static let EPFNOSUPPORT = ErrNo(rawValue: Darwin.C.errno.EPFNOSUPPORT)
+	static let EPFNOSUPPORT = ErrNo(rawValue: Darwin.EPFNOSUPPORT)
 	/// Address family not supported by protocol family
-	static let EAFNOSUPPORT = ErrNo(rawValue: Darwin.C.errno.EAFNOSUPPORT)
+	static let EAFNOSUPPORT = ErrNo(rawValue: Darwin.EAFNOSUPPORT)
 	/// Address already in use
-	static let EADDRINUSE = ErrNo(rawValue: Darwin.C.errno.EADDRINUSE)
+	static let EADDRINUSE = ErrNo(rawValue: Darwin.EADDRINUSE)
 	/// Can't assign requested address
-	static let EADDRNOTAVAIL = ErrNo(rawValue: Darwin.C.errno.EADDRNOTAVAIL)
+	static let EADDRNOTAVAIL = ErrNo(rawValue: Darwin.EADDRNOTAVAIL)
 
 /* ipc/network software -- operational errors */
 	/// Network is down
-	static let ENETDOWN = ErrNo(rawValue: Darwin.C.errno.ENETDOWN)
+	static let ENETDOWN = ErrNo(rawValue: Darwin.ENETDOWN)
 	/// Network is unreachable
-	static let ENETUNREACH = ErrNo(rawValue: Darwin.C.errno.ENETUNREACH)
+	static let ENETUNREACH = ErrNo(rawValue: Darwin.ENETUNREACH)
 	/// Network dropped connection on reset
-	static let ENETRESET = ErrNo(rawValue: Darwin.C.errno.ENETRESET)
+	static let ENETRESET = ErrNo(rawValue: Darwin.ENETRESET)
 	/// Software caused connection abort
-	static let ECONNABORTED = ErrNo(rawValue: Darwin.C.errno.ECONNABORTED)
+	static let ECONNABORTED = ErrNo(rawValue: Darwin.ECONNABORTED)
 	/// Connection reset by peer
-	static let ECONNRESET = ErrNo(rawValue: Darwin.C.errno.ECONNRESET)
+	static let ECONNRESET = ErrNo(rawValue: Darwin.ECONNRESET)
 	/// No buffer space available
-	static let ENOBUFS = ErrNo(rawValue: Darwin.C.errno.ENOBUFS)
+	static let ENOBUFS = ErrNo(rawValue: Darwin.ENOBUFS)
 	/// Socket is already connected
-	static let EISCONN = ErrNo(rawValue: Darwin.C.errno.EISCONN)
+	static let EISCONN = ErrNo(rawValue: Darwin.EISCONN)
 	/// Socket is not connected
-	static let ENOTCONN = ErrNo(rawValue: Darwin.C.errno.ENOTCONN)
+	static let ENOTCONN = ErrNo(rawValue: Darwin.ENOTCONN)
 	/// Can't send after socket shutdown
-	static let ESHUTDOWN = ErrNo(rawValue: Darwin.C.errno.ESHUTDOWN)
+	static let ESHUTDOWN = ErrNo(rawValue: Darwin.ESHUTDOWN)
 	/// Too many references: can't splice
-	static let ETOOMANYREFS = ErrNo(rawValue: Darwin.C.errno.ETOOMANYREFS)
+	static let ETOOMANYREFS = ErrNo(rawValue: Darwin.ETOOMANYREFS)
 	/// Operation timed out
-	static let ETIMEDOUT = ErrNo(rawValue: Darwin.C.errno.ETIMEDOUT)
+	static let ETIMEDOUT = ErrNo(rawValue: Darwin.ETIMEDOUT)
 	/// Connection refused
-	static let ECONNREFUSED = ErrNo(rawValue: Darwin.C.errno.ECONNREFUSED)
+	static let ECONNREFUSED = ErrNo(rawValue: Darwin.ECONNREFUSED)
 
 	/// Too many levels of symbolic links
-	static let ELOOP = ErrNo(rawValue: Darwin.C.errno.ELOOP)
+	static let ELOOP = ErrNo(rawValue: Darwin.ELOOP)
 	/// File name too long
-	static let ENAMETOOLONG = ErrNo(rawValue: Darwin.C.errno.ENAMETOOLONG)
+	static let ENAMETOOLONG = ErrNo(rawValue: Darwin.ENAMETOOLONG)
 
 	/// Host is down
-	static let EHOSTDOWN = ErrNo(rawValue: Darwin.C.errno.EHOSTDOWN)
+	static let EHOSTDOWN = ErrNo(rawValue: Darwin.EHOSTDOWN)
 	/// No route to host
-	static let EHOSTUNREACH = ErrNo(rawValue: Darwin.C.errno.EHOSTUNREACH)
+	static let EHOSTUNREACH = ErrNo(rawValue: Darwin.EHOSTUNREACH)
 	/// Directory not empty
-	static let ENOTEMPTY = ErrNo(rawValue: Darwin.C.errno.ENOTEMPTY)
+	static let ENOTEMPTY = ErrNo(rawValue: Darwin.ENOTEMPTY)
 
 /* quotas & mush */
 	/// Too many processes
-	static let EPROCLIM = ErrNo(rawValue: Darwin.C.errno.EPROCLIM)
+	static let EPROCLIM = ErrNo(rawValue: Darwin.EPROCLIM)
 	/// Too many users
-	static let EUSERS = ErrNo(rawValue: Darwin.C.errno.EUSERS)
+	static let EUSERS = ErrNo(rawValue: Darwin.EUSERS)
 	/// Disc quota exceeded
-	static let EDQUOT = ErrNo(rawValue: Darwin.C.errno.EDQUOT)
+	static let EDQUOT = ErrNo(rawValue: Darwin.EDQUOT)
 
 /* Network File System */
 	/// Stale NFS file handle
-	static let ESTALE = ErrNo(rawValue: Darwin.C.errno.ESTALE)
+	static let ESTALE = ErrNo(rawValue: Darwin.ESTALE)
 	/// Too many levels of remote in path
-	static let EREMOTE = ErrNo(rawValue: Darwin.C.errno.EREMOTE)
+	static let EREMOTE = ErrNo(rawValue: Darwin.EREMOTE)
 	/// RPC struct is bad
-	static let EBADRPC = ErrNo(rawValue: Darwin.C.errno.EBADRPC)
+	static let EBADRPC = ErrNo(rawValue: Darwin.EBADRPC)
 	/// RPC version wrong
-	static let ERPCMISMATCH = ErrNo(rawValue: Darwin.C.errno.ERPCMISMATCH)
+	static let ERPCMISMATCH = ErrNo(rawValue: Darwin.ERPCMISMATCH)
 	/// RPC prog. not avail
-	static let EPROGUNAVAIL = ErrNo(rawValue: Darwin.C.errno.EPROGUNAVAIL)
+	static let EPROGUNAVAIL = ErrNo(rawValue: Darwin.EPROGUNAVAIL)
 	/// Program version wrong
-	static let EPROGMISMATCH = ErrNo(rawValue: Darwin.C.errno.EPROGMISMATCH)
+	static let EPROGMISMATCH = ErrNo(rawValue: Darwin.EPROGMISMATCH)
 	/// Bad procedure for program
-	static let EPROCUNAVAIL = ErrNo(rawValue: Darwin.C.errno.EPROCUNAVAIL)
+	static let EPROCUNAVAIL = ErrNo(rawValue: Darwin.EPROCUNAVAIL)
 
 	/// No locks available
-	static let ENOLCK = ErrNo(rawValue: Darwin.C.errno.ENOLCK)
+	static let ENOLCK = ErrNo(rawValue: Darwin.ENOLCK)
 	/// Function not implemented
-	static let ENOSYS = ErrNo(rawValue: Darwin.C.errno.ENOSYS)
+	static let ENOSYS = ErrNo(rawValue: Darwin.ENOSYS)
 
 	/// Inappropriate file type or format
-	static let EFTYPE = ErrNo(rawValue: Darwin.C.errno.EFTYPE)
+	static let EFTYPE = ErrNo(rawValue: Darwin.EFTYPE)
 	/// Authentication error
-	static let EAUTH = ErrNo(rawValue: Darwin.C.errno.EAUTH)
+	static let EAUTH = ErrNo(rawValue: Darwin.EAUTH)
 	/// Need authenticator
-	static let ENEEDAUTH = ErrNo(rawValue: Darwin.C.errno.ENEEDAUTH)
+	static let ENEEDAUTH = ErrNo(rawValue: Darwin.ENEEDAUTH)
 
 /* Intelligent device errors */
 	/// Device power is off
-	static let EPWROFF = ErrNo(rawValue: Darwin.C.errno.EPWROFF)
+	static let EPWROFF = ErrNo(rawValue: Darwin.EPWROFF)
 	/// Device error, e.g. paper out
-	static let EDEVERR = ErrNo(rawValue: Darwin.C.errno.EDEVERR)
+	static let EDEVERR = ErrNo(rawValue: Darwin.EDEVERR)
 
 	/// Value too large to be stored in data type
-	static let EOVERFLOW = ErrNo(rawValue: Darwin.C.errno.EOVERFLOW)
+	static let EOVERFLOW = ErrNo(rawValue: Darwin.EOVERFLOW)
 
 /* Program loading errors */
 	/// Bad executable
-	static let EBADEXEC = ErrNo(rawValue: Darwin.C.errno.EBADEXEC)
+	static let EBADEXEC = ErrNo(rawValue: Darwin.EBADEXEC)
 	/// Bad CPU type in executable
-	static let EBADARCH = ErrNo(rawValue: Darwin.C.errno.EBADARCH)
+	static let EBADARCH = ErrNo(rawValue: Darwin.EBADARCH)
 	/// Shared library version mismatch
-	static let ESHLIBVERS = ErrNo(rawValue: Darwin.C.errno.ESHLIBVERS)
+	static let ESHLIBVERS = ErrNo(rawValue: Darwin.ESHLIBVERS)
 	/// Malformed Macho file
-	static let EBADMACHO = ErrNo(rawValue: Darwin.C.errno.EBADMACHO)
+	static let EBADMACHO = ErrNo(rawValue: Darwin.EBADMACHO)
 
 	/// Operation canceled
-	static let ECANCELED = ErrNo(rawValue: Darwin.C.errno.ECANCELED)
+	static let ECANCELED = ErrNo(rawValue: Darwin.ECANCELED)
 
 	/// Identifier removed
-	static let EIDRM = ErrNo(rawValue: Darwin.C.errno.EIDRM)
+	static let EIDRM = ErrNo(rawValue: Darwin.EIDRM)
 	/// No message of desired type
-	static let ENOMSG = ErrNo(rawValue: Darwin.C.errno.ENOMSG)   
+	static let ENOMSG = ErrNo(rawValue: Darwin.ENOMSG)   
 	/// Illegal byte sequence
-	static let EILSEQ = ErrNo(rawValue: Darwin.C.errno.EILSEQ)
+	static let EILSEQ = ErrNo(rawValue: Darwin.EILSEQ)
 	/// Attribute not found
-	static let ENOATTR = ErrNo(rawValue: Darwin.C.errno.ENOATTR)
+	static let ENOATTR = ErrNo(rawValue: Darwin.ENOATTR)
 
 	/// Bad message
-	static let EBADMSG = ErrNo(rawValue: Darwin.C.errno.EBADMSG)
+	static let EBADMSG = ErrNo(rawValue: Darwin.EBADMSG)
 	/// Reserved
-	static let EMULTIHOP = ErrNo(rawValue: Darwin.C.errno.EMULTIHOP)
+	static let EMULTIHOP = ErrNo(rawValue: Darwin.EMULTIHOP)
 	/// No message available on STREAM
-	static let ENODATA = ErrNo(rawValue: Darwin.C.errno.ENODATA)
+	static let ENODATA = ErrNo(rawValue: Darwin.ENODATA)
 	/// Reserved
-	static let ENOLINK = ErrNo(rawValue: Darwin.C.errno.ENOLINK)
+	static let ENOLINK = ErrNo(rawValue: Darwin.ENOLINK)
 	/// No STREAM resources
-	static let ENOSR = ErrNo(rawValue: Darwin.C.errno.ENOSR)
+	static let ENOSR = ErrNo(rawValue: Darwin.ENOSR)
 	/// Not a STREAM
-	static let ENOSTR = ErrNo(rawValue: Darwin.C.errno.ENOSTR)
+	static let ENOSTR = ErrNo(rawValue: Darwin.ENOSTR)
 	/// Protocol error
-	static let EPROTO = ErrNo(rawValue: Darwin.C.errno.EPROTO)
+	static let EPROTO = ErrNo(rawValue: Darwin.EPROTO)
 	/// STREAM ioctl timeout
-	static let ETIME = ErrNo(rawValue: Darwin.C.errno.ETIME)
-
-	/// Operation not supported on socket
-	static let EOPNOTSUPP = ErrNo(rawValue: Darwin.C.errno.EOPNOTSUPP)
+	static let ETIME = ErrNo(rawValue: Darwin.ETIME)
 
 	/// No such policy registered
-	static let ENOPOLICY = ErrNo(rawValue: Darwin.C.errno.ENOPOLICY)
+	static let ENOPOLICY = ErrNo(rawValue: Darwin.ENOPOLICY)
 
 	/// Must be equal largest errno
-	static let ELAST = ErrNo(rawValue: Darwin.C.errno.ELAST)
-
-/* pseudo-errors returned inside kernel to modify return to process */
-	/// restart syscall
-	static let ERESTART = ErrNo(rawValue: Darwin.C.errno.ERESTART)
-	/// don't modify regs, just return
-	static let EJUSTRETURN = ErrNo(rawValue: Darwin.C.errno.EJUSTRETURN)
-	/// restart lookup under heavy vnode pressure/recycling
-	static let ERECYCLE = ErrNo(rawValue: Darwin.C.errno.ERECYCLE)
-	/// restart with the drive open
-	static let EREDRIVEOPEN = ErrNo(rawValue: Darwin.C.errno.EREDRIVEOPEN)
+	static let ELAST = ErrNo(rawValue: Darwin.ELAST)
 }
 #endif

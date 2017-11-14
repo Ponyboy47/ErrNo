41aa86959fdb8456d16a418e65ee1850f04c0268
Jacob Williams <ponyboy47@gmail.com>
2017-11-14 09:28:43 -0700

    Verify functionality for swift 4

--- Package.swift
+++ Package.swift
@@ -1,6 +1,19 @@
+// swift-tools-version:4.0
+
 import PackageDescription
 
 let package = Package(
-    name: "ErrNo"
+    name: "ErrNo",
+    products: [
+        .library(
+            name: "ErrNo",
+            targets: ["ErrNo"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(name: "ErrNo",
+            dependencies: []),
+    ]
 )
 
